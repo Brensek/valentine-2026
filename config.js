f78e73d39b96c2bea3bdcaa04c2f90efae0cee1d
zen <74750702+Brensek@users.noreply.github.com>
2026-02-12 14:09:51 -0800

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yayy! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Giving you the biggest virtual hugs and kisses 🥰👩‍❤️‍💋‍👨😘 Flowers and gifts are waiting for you the next time we meet!",
+        message: "Giving you the biggest virtual hugs and kisses!! 🥰👩‍❤️‍💋‍👨😘 Flowers and gifts are waiting for you the next time we meet!! 💐😘",
         emojis: "🎁💖🤗💝💋❤️💕🍫"  // These will bounce around
     },
 
